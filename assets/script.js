9542143df76d6ca28a5c334c369fe1b6ac7ce71c
pipthedev <pip@rabbitwebdesign.co.uk>
2025-04-16 20:55:22 +0100

    v2

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,7 @@ jQuery(document).ready(function($) {
         $(this).closest('.wse-step').hide().prev().show();
     });
 });
+jQuery(document).ready(function($) {
+    $('.wse-step').hide(); // Hide all steps initially
+    $('.wse-step').first().show(); // Show the first step
+});
